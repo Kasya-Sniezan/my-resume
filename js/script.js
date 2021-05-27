6446f8168295dd4070e3e6711c3c25d8f0a3925c
Kasya Sniezan <kasya.sniezan@gmail.com>
2021-05-27 19:46:28 +0300

    add spoiler-block

--- js/script.js
+++ js/script.js
@@ -76,3 +76,13 @@ if (menuLinks.length > 0) {
         }
     }
 }
+
+/*Добавление слайдера*/
+const rowArrow = document.querySelector('.row--skills-block');
+const spoilerBlock = document.querySelector('.spoiler-wrap');
+if (rowArrow){
+    rowArrow.addEventListener("click", function (e) {
+        rowArrow.classList.toggle('_active');
+        spoilerBlock.classList.toggle('_active');
+    });
+}
