a2c82c2c46cbe6bf4f42dcb5dbd6d04ae63d82b4
Kasya Sniezan <kasya.sniezan@gmail.com>
2021-10-27 20:56:51 +0300

    add slick slider

--- #source/js/main.js
+++ #source/js/main.js
@@ -2,4 +2,14 @@
 //@prepros-append add-spoiler.js
 //@prepros-append def-system.js
 //@prepros-append go-to-block.js
-//@prepros-append slider.js
+//@prepros-append slick.min.js
+
+$(document).ready(function () {
+  $(".slick-slider").slick({
+    arrows: false,
+    infinite: true,
+    slidesToShow: 1,
+    dots: true,
+    adaptiveHeight: true,
+  });
+});
